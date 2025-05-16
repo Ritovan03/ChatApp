88cd3502626b31102be1422b8ce0a5427c9b65cc
Ritovan03 <ritovan0302@gmail.com>
2025-05-16 16:01:49 +0530

    feat: implement socket.io server and update CORS settings for frontend integration

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
