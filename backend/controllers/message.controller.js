bffade8efff488565e1a31aaa2a013c8b9032dc6
Ritovan03 <ritovan0302@gmail.com>
2025-05-16 17:41:12 +0530

    feat: add message model, controller, and routes for user messaging functionality

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/User.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try{
+        const loggedInUserID = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserID}}).select("-password");
+
+        res.status(200).json(filteredUsers);
+
+    } catch(err){
+        console.log(err);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try{
+        
+    } catch(err){
+        console.log(err);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+
---
+++ backend/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware';
+import { getUsersForSidebar,getMessages } from '../controllers/message.controller.js';
+
+
+const router = express.Router();
+
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+export default router;
