23c6eb9716542377ecaf41013d9b5bb36760c707
Ritovan03 <ritovan0302@gmail.com>
2025-05-17 16:34:26 +0530

    feat: add routing and pages for user navigation, integrate daisyUI for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to the App</h1>
-      <p>This is a simple React application.</p>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
-}
+};
 export default App;
